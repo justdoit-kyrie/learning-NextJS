3f1fc8f7a09066d798f887c64dd0ddaedbf63f92
justdoit-kyrie <ducndmse151198@gmail.com>
2021-12-28 21:36:31 +0700

    init project first time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const goToDetailPage = () => {
+    router.push({
+      pathname: "/posts/[postId]",
+      query: { postId: 123, userId: 456 },
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,9 +29,15 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+
+        <Link href="/about">
+          <a>Go to about page</a>
+        </Link>
+
+        <button onClick={goToDetailPage}>go to detail page</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -59,14 +76,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ pages/posts/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface PostsListPageProps {}
+
+export default function PostsListPage(props: PostsListPageProps) {
+  return <div>Post List Page</div>;
+}
