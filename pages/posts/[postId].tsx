3ccc7e1b3ab6c8785dac9af29950ee063be3120b
justdoit-kyrie <ducndmse151198@gmail.com>
2021-12-29 20:35:17 +0700

    update prettier

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,7 +1,58 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import * as React from "react";
 
-export interface PostDetailPageProps {}
-
-export default function PostDetailPage(props: PostDetailPageProps) {
-  return <div>Post Detail Page</div>;
+export interface PostDetailPageProps {
+  post: any;
 }
+
+export default function PostDetailPage({ post }: PostDetailPageProps) {
+  return (
+    <div>
+      <h1>Post Detail Page</h1>
+
+      <p>Post detail</p>
+      <p>{post.title}</p>
+      <p>{post.description}</p>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch(`https://js-post-api.herokuapp.com/api/posts`);
+
+  const data = await response.json();
+
+  const newData = data.map((post: any) => ({
+    params: {
+      postId: post.id,
+    },
+  }));
+
+  return {
+    paths: newData,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const postId = context.params?.postId;
+  console.log("GET STATIC PROPS", postId);
+  if (!postId)
+    return {
+      props: {},
+    };
+
+  const response = await fetch(
+    `https://js-post-api.herokuapp.com/api/posts/${postId}`
+  );
+
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,70 @@
-import * as React from "react";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 export interface PostsListPageProps {}
 
 export default function PostsListPage(props: PostsListPageProps) {
-  return <div>Post List Page</div>;
+  const router = useRouter();
+  const [postList, setPostList] = useState([]);
+  const page = Number(router.query.page);
+
+  console.log("Router query", router.query);
+
+  const handleNextPage = () => {
+    router.push(
+      {
+        pathname: "/posts",
+        query: {
+          page: (page || 1) + 1,
+        },
+      },
+      undefined,
+      {
+        shallow: true,
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (!page) return;
+
+    (async () => {
+      const response = await fetch(
+        `https://js-post-api.herokuapp.com/api/posts?_page=${page}`
+      );
+
+      const { data } = await response.json();
+
+      setPostList(data);
+    })();
+  }, [page]);
+
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <ul className="post-list">
+        {postList.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleNextPage}>Next Page</button>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  console.log("GET STATIC PROPS");
+
+  return {
+    props: {},
+  };
+};
